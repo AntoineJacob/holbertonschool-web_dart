c1447c403ba7490a7368160863db3f52c05f79fd
AntoineJacob <5646@holbertonstudents.com>
2024-11-15 11:59:12 +0100

    fixing the password file

--- 0x02-dart-classes/6-password.dart
+++ 0x02-dart-classes/6-password.dart
@@ -1,33 +1,31 @@
 class Password {
-
   String _password;
-  Password({required String password}) : _password = password;
-  
+
+  // Constructeur avec une vérification null
+  Password({String? password}) : _password = password ?? ''; // Utilise '' comme valeur par défaut si `password` est null
+
   String get password => _password;
 
-   void set password(String password) {
-    this._password = password;
+  void set password(String password) {
+    _password = password;
   }
 
   bool isValid() {
-     return ((password.length >= 8) &&
-     (password.length <= 16)) &&
-        this.password.contains(new RegExp(r"[A-Z]"))
-	&& this.password.contains(new RegExp(r"[a-z]"))
-	&& this.password.contains(new RegExp(r"[0-9]"));
+    // Vérifie si le mot de passe n'est pas null et a une longueur adéquate
+    if (_password.isNotEmpty && _password.length >= 8 && _password.length <= 16) {
+      // Vérification des exigences de la validité du mot de passe
+      if (_password.contains(RegExp(r'[A-Z]')) &&
+          _password.contains(RegExp(r'[a-z]')) &&
+          _password.contains(RegExp(r'[0-9]'))) {
+        return true;
+      }
+    }
+
+    return false;
   }
 
   @override
-  String toString() => "Your Password is: ${this.password}";
-}
-
-
-
-void main() {
-  final password = Password(password: "Passwordecode");
-  print(password.isValid());
-  print(password.toString());
-  password.password = "Youssef4321";
-  print(password.isValid());
-  print(password.toString());
+  String toString() {
+    return 'Your Password is: $_password';
+  }
 }
