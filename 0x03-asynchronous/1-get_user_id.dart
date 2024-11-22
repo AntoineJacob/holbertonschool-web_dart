cf30df7ce94c1e2345bb41f2e0f3c938d5d3d611
AntoineJacob <5646@holbertonstudents.com>
2024-11-22 14:48:58 +0100

    redo task 1

--- 0x03-asynchronous/1-get_user_id.dart
+++ 0x03-asynchronous/1-get_user_id.dart
@@ -1,14 +1,20 @@
-import 'dart:async';
-import 'dart:convert';
+import 'dart:convert'; // Pour utiliser jsonDecode
+import '1-util.dart'; // Importation de fetchUserData
 
-Future<String> fetchUserData() => Future.delayed(
-      const Duration(seconds: 2),
-      () =>
-          '{"id" : "7ee9a243-01ca-47c9-aa14-0149789764c3", "username" : "admin"}',
-    );
-
+// Fonction pour obtenir l'ID utilisateur
 Future<String> getUserId() async {
-  String userData = await fetchUserData();
-  final Map<String, dynamic> userMap = jsonDecode(userData);
-  return userMap['id'] as String;
+  try {
+    // Récupère les données utilisateur sous forme de chaîne JSON
+    String userData = await fetchUserData();
+
+    // Décode la chaîne JSON en Map
+    Map<String, dynamic> userMap = jsonDecode(userData);
+
+    // Retourne l'ID de l'utilisateur
+    return userMap['id'];
+  } catch (e) {
+    // Gère les erreurs en cas de problème
+    print("Error fetching user ID: $e");
+    return ''; // Retourne une chaîne vide en cas d'erreur
+  }
 }
